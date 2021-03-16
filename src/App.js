1ba143731d0b276cbae5aeda3a47123da62edaa1
Pragya <pjilowa@ce.iitr.ac.in>
2021-03-16 13:26:56 +0530

    some
    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import image from "./images/nav_image.jpg";
-import Navbar from "./navbar";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Template from "./template.js";
-import Cards from "./cards.js";
-import Topics from "./topics.js";
-import Footer from "./footer.js"
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Cards from "./cards.js"
+import Footer from "../src/components/footer"
+import image from "./images/nav_image.jpg"
+import Navbar from "./components/navbar"
+import Pagination from "./components/pagination"
+import Topics from "./components/topics"
+import Template from "./components/template"
+
 
 function App() {
   return (
@@ -20,6 +22,7 @@ function App() {
             </div>
             <Topics />
             <Cards />
+            <Pagination/>
             <Footer/>
           </Route>
           <Route exact path="/template/:id" component={Template} />
